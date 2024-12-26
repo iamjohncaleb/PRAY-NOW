a9474b7e2d1eb656b2a2af717025175593f9b8a4
Caleb John <johncaleb022@gmail.com>
2024-12-26 10:45:06 +0100

    Update AuthContext.js

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -26,7 +26,6 @@
 
 
 
-
 'use client'
 import { auth, db } from '@/firebase'
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
